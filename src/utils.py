ab63d055bc8f94a5ce29dcee50d03ea4f1e3ec96
Alexey Bauman <alxgenba@gmail.com>
2019-06-14 20:50:25 +0300

    fix norm

--- src/utils.py
+++ src/utils.py
@@ -5,5 +5,8 @@ def orth(A):
     A_orth, _ = np.linalg.qr(A)
     return A_orth
 
-def norm(A, ord='fro'):
-    return np.linalg.norm(A, ord=ord)
+def norm(A, ord=None):
+    if ord is None:
+        return np.linalg.norm(A)
+    else:
+        return np.linalg.norm(A, ord=ord)
